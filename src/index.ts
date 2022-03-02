e6387a2be44da4dd38b5da65ce540b79f59f2ea9
Ritik singh <44609236+itsritiksingh@users.noreply.github.com>
2022-03-02 22:42:10 +0530

    fetch file apis added

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,23 @@
 import express from 'express';
 import { metacall_inspect } from 'metacall';
 import { callFnByName, fetchFiles } from './utils';
-export const startDevServer = (): void => {
-	const app = express();
+const app = express();
 
-	app.use(express.json());
-	app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
-	app.get('/inspect', (req, res) => {
-		res.send(metacall_inspect());
-	});
-	app.post('/call/:name', callFnByName);
+app.get('/inspect', (req, res) => {
+	res.send(metacall_inspect());
+});
+app.post('/call/:name', callFnByName);
 
-	app.post('/api/repository/add', fetchFiles);
+app.post('/api/package/create', fetchFiles);
+//TODO fetch from repository
+// app.post('/api/repository/add', fetchFilesFromRepo);
 
-	app.listen(9000, () => {
-		console.log('Server listening...');
-	});
-};
+//TODO load script and install their packages
+//TODO server fn via api
+
+app.listen(9000, () => {
+	console.log('Server listening...');
+});
--- src/utils.ts
+++ src/utils.ts
@@ -4,6 +4,9 @@ import os from 'os';
 import * as path from 'path';
 import * as fs from 'fs';
 import busboy, { Busboy } from 'busboy';
+import { currentFile, namearg, valueArg } from './constants';
+import clone from 'git-clone/promise';
+import crypto from 'crypto';
 
 export const callFnByName = (req: Request, res: Response): Response => {
 	if (!(req.params && req.params.name)) {
@@ -19,22 +22,36 @@ export const callFnByName = (req: Request, res: Response): Response => {
 
 export const fetchFiles = (req: Request, res: Response): Busboy => {
 	const bb = busboy({ headers: req.headers });
-	bb.on('file', (name, file, info) => {
-		const { filename, encoding, mimeType } = info;
-		console.log(
-			`File [${name}]: filename: %j, encoding: %j, mimeType: %j`,
-			filename,
-			encoding,
-			mimeType
-		);
-		const saveTo = path.join(os.tmpdir(), `${filename}`);
+	bb.on('file', (name, file) => {
+		const saveTo = path.join(os.tmpdir(), 'metacall', `${name}`);
+		console.log(saveTo);
 		file.pipe(fs.createWriteStream(saveTo));
 	});
-	bb.on('field', (name, val) => {
-		console.log(`Field [${name}]: value: %j`, val);
+
+	bb.on('field', (name: namearg, val: valueArg) => {
+		currentFile[name] = val;
 	});
 	bb.on('close', () => {
+		currentFile.path = path.join(
+			os.tmpdir(),
+			'metacall',
+			`${currentFile.id as string}`
+		);
 		res.end();
 	});
 	return req.pipe(bb);
+};
+
+export const fetchFilesFromRepo = async (
+	req: Omit<Request, 'body'> & { body: 'branch' & 'url' },
+	res: Response
+): Promise<Response> => {
+	const { branch, url } = req.body;
+	const filename = `${crypto.randomBytes(4).toString('hex')}.zip`;
+
+	await clone(url, path.join(os.tmpdir(), 'metacall', filename), {
+		checkout: branch
+	});
+
+	return res.json({ id: filename });
 };
