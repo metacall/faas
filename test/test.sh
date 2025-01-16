4f5de396d478fba7c4b29ca868679fe2543e2b58
Himanshu Sharma <himanshu.kumarr07@gmail.com>
2025-01-16 23:28:33 +0530

    Reverted test.sh

--- test/test.sh
+++ test/test.sh
@@ -18,7 +18,7 @@
 #	See the License for the specific language governing permissions and
 #	limitations under the License.
 
-set -euo pipefail
+set -exuo pipefail
 
 # Maximum number of retries
 MAX_RETRIES=5
