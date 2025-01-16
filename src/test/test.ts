ce0bf329f887f5c42bc91f987ab964bdb42ac52e
Himanshu Sharma <himanshu.kumarr07@gmail.com>
2025-01-16 23:26:26 +0530

    Reverted -x

--- src/test/test.ts
+++ src/test/test.ts
@@ -1,5 +1,7 @@
+/* eslint-disable @typescript-eslint/no-unsafe-call */
 import { strict as assert } from 'assert';
 
+// eslint-disable-next-line @typescript-eslint/no-unsafe-call
 describe('TODO', function () {
 	it('Implement unit testing for the components needed', () => {
 		assert.strictEqual([1, 2, 3].indexOf(4), -1);
