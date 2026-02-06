ce61fc384371007bb693b8f34b8923a99e4bd114
gotnochill815-web <gotnochill815@gmail.com>
2026-02-06 21:17:16 +0530

    test: add unit test for version utility

--- src/test/test.ts
+++ src/test/test.ts
@@ -1,7 +1,17 @@
 import { strict as assert } from 'assert';
+import { writeFileSync, unlinkSync } from 'fs';
+import { join } from 'path';
+import { getVersion } from '../utils/version';
 
-describe('TODO', function () {
-	it('Implement unit testing for the components needed', () => {
-		assert.strictEqual([1, 2, 3].indexOf(4), -1);
+describe('utils/version', () => {
+	it('reads version from package.json', () => {
+		const tmpPath = join(__dirname, 'tmp-package.json');
+
+		writeFileSync(tmpPath, JSON.stringify({ version: '1.2.3' }));
+
+		const version = getVersion(tmpPath);
+		assert.equal(version, '1.2.3');
+
+		unlinkSync(tmpPath);
 	});
 });
